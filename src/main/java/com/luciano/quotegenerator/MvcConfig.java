207dce31eaf6e4750cb4cb6992f4fde0a059b228
Luciano Gabbanelli <luciano.gabbanelli@globant.com>
2019-02-13 17:53:23 -0300

    autenticacion

---
+++ src/main/java/com/luciano/quotegenerator/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.luciano.quotegenerator;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer
+{
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry)
+    {
+        registry.addViewController("/home").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/v1/quotes").setViewName("quotes");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
